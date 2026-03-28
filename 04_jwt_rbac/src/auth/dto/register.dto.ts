2cc000fdd847ce2c5aa9861804b4309bba1b7b08
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-03-28 22:02:12 +0530

    Add RegisterDto for user registration with validation rules

---
+++ 04_jwt_rbac/src/auth/dto/register.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail({}, { message: 'Invalid email address' })
+  @IsString({ message: 'Email must be a string' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @MaxLength(100, { message: 'Password must be at most 100 characters long' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must be a string' })
+  @MinLength(3, { message: 'Name must be at least 2 characters long' })
+  @MaxLength(50, { message: 'Name must be at most 50 characters long' })
+  name: string;
+}
